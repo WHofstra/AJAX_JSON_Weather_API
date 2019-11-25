6a14d09417ac341be1379690b62d0855867dd2b1
Wayne Hofstra <wayne.hofstra@hva.nl>
2019-11-25 10:18:59 +0100

    Added getWeather1

--- script.js
+++ script.js
@@ -15,3 +15,47 @@ let key = "demo";
 let locatie = "&locatie=";
 let geoLocation = "Amsterdam"; //Locatie als string
 let url = apiAddress + key + locatie + geoLocation; //URL van API
+
+function getWeather1(){
+  console.log(url);
+  makeAjaxCall(url, "GET"). then(showWeather1, errorHandler);
+}
+
+function showWeather1(JSONresponseFromAjax){
+  result.innerHTML = JSONresponseFromAjax; //Laat de ruwe data zien
+}
+
+function makeAjaxCall(url, methodType){
+  let promiseObj = new Promise(function(resolve, reject){
+    console.log(url);
+
+    let xmlhttp = new XMLHttpRequest();
+    xmlhttp.open(methodType, url, true);
+    xmlhttp.send();
+
+    xmlhttp.onreadystatechange = function(){
+      if (xmlhttp.readystate === 4)
+      {
+        if (xmlhttp.status === 200)
+        {
+          console.log("Xmlhttp done succesfully.");
+
+          let serverResponse = xmlhttp.responseText; //Server antwoord met string
+          console.log(serverResponse);
+          resolve(serverResponse); //Wordt via return promiseObj teruggegeven
+        }
+        else
+        {
+          reject(xmlhttp);
+          console.log("Xmlhttp processing still going on.");
+        }
+      }
+    }
+    console.log("Request sent succesfully.");
+  });
+  return promiseObj;
+}
+
+function errorHandler(statusCode){
+  console.log("Failed with status ", status);
+}
