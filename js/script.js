649dc54549dc21fcdc5cd100afb58e58e20c6020
Wayne Hofstra <wayne.hofstra@hva.nl>
2019-11-26 13:17:32 +0100

    Files Added

---
+++ js/script.js
@@ -0,0 +1,26 @@
+//HTML elementen
+let activate1 = document.getElementById('activate1'); //Bind via DOM
+let activate2 = document.getElementById('activate2'); //Bind via DOM aan JS
+let activate3 = document.getElementById('activate3'); //Bind via DOMbutton aan JS
+
+activate1.addEventListener("click", getWeather1); //Click Event
+
+
+
+let result = document.getElementById('result'); //Bind div element aan JS
+
+//Weather API http://weerlive.nl/delen.php
+let apiAddress = "http://weerlive.nl/api/json-data-10min.php?key=" //Address
+let key = "demo";
+let locatie = "&locatie=";
+let geoLocation = "Amsterdam"; //Locatie als string
+let url = apiAddress + key + locatie + geoLocation; //URL van API
+
+function getWeather1(){
+  console.log(url);
+  makeAjaxCall(url, "GET"). then(showWeather1, errorHandler);
+}
+
+function showWeather1(JSONresponseFromAjax){
+  result.innerHTML = JSONresponseFromAjax; //Laat de ruwe data zien
+}
